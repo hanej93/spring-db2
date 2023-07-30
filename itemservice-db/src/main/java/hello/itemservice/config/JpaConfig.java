77cb948e734f1095d5f47751492bb1d89afb4d61
EJ <hanej93@naver.com>
2023-07-30 12:59:00 +0900

    JPA 적용1 - 개발

---
+++ itemservice-db/src/main/java/hello/itemservice/config/JpaConfig.java
@@ -0,0 +1,31 @@
+package hello.itemservice.config;
+
+import javax.persistence.EntityManager;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.jpa.JpaItemRepositoryV1;
+import hello.itemservice.repository.mybatis.ItemMapper;
+import hello.itemservice.repository.mybatis.MybatisItemRepository;
+import hello.itemservice.service.ItemService;
+import hello.itemservice.service.ItemServiceV1;
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class JpaConfig {
+
+	private final EntityManager em;
+
+	@Bean
+	public ItemService itemService() {
+		return new ItemServiceV1(itemRepository());
+	}
+
+	@Bean
+	public ItemRepository itemRepository() {
+		return new JpaItemRepositoryV1(em);
+	}
+}
