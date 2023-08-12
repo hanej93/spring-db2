77f958b97d3a2bc3361bfa44f8c67977b3dea7a2
EJ <hanej93@naver.com>
2023-08-13 01:36:37 +0900

    트랜잭션 전파 활용7 - 복구 REQUIRES_NEW

--- springtx/src/main/java/hello/springtx/propagation/LogRepository.java
+++ springtx/src/main/java/hello/springtx/propagation/LogRepository.java
@@ -3,6 +3,7 @@ package hello.springtx.propagation;
 import java.util.Optional;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import jakarta.persistence.EntityManager;
@@ -16,7 +17,7 @@ public class LogRepository {
 
 	private final EntityManager em;
 
-	@Transactional
+	@Transactional(propagation = Propagation.REQUIRES_NEW)
 	public void save(Log logMessage) {
 		log.info("log 저장");
 		em.persist(logMessage);
--- springtx/src/main/java/hello/springtx/propagation/MemberService.java
+++ springtx/src/main/java/hello/springtx/propagation/MemberService.java
@@ -1,6 +1,7 @@
 package hello.springtx.propagation;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
